f9536e0d7ae45e79c9d922d7c76879e453c6e8ea
DJ EDY <zetaactivo1963@gmail.com>
2026-02-01 00:39:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css"; // <-- OJO: aquí va styles.css
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./styles.css";
 
-
-const root = createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
