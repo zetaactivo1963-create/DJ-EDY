4a86119ada7e5876986dda32d374318f3ea71f39
DJ EDY <zetaactivo1963@gmail.com>
2026-02-01 00:11:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css"; // <-- OJO: aquí va styles.css
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
