a9a59264c31b3f897d3b90ee5ba6a7edfed9fa96
DJ EDY <zetaactivo1963@gmail.com>
2026-02-16 01:37:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1084,7 +1084,7 @@ const serviciosAdicionales = [
                   className="absolute inset-0 bg-cover bg-center"
                   style={{ backgroundImage: `url(${setup.image})` }}
                 />
-                <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
+                <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
               </div>
 
               {/* Info */}
